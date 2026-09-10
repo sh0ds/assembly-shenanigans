4c6e213d719e00d729428fb7008f5c190d81c841
sh0ds <mail@sh0ds.dev>
2026-09-10 16:29:49 +0200

    added input to string reversal, handled edge case of just 1 \n

--- string-reversal/main.asm
+++ string-reversal/main.asm
@@ -1,18 +1,75 @@
-section .data
-    string db 'Hello, World!'
-    strLen equ $ - string
+segment .data
+    prompt db 'Please input a string:', 0x0a
+    prLen equ $ - prompt
+    yourString db 'Your String:', 0x0a
+    ySLen equ $ - yourString
+    newline db 0x0a
+    ySReversed db 'Your reversed string:', 0x0a
+    ySRLen equ $ - ySReversed
 
-section .bss
-    revString: resb strLen
+segment .bss
+    input: resb 64
+    revString: resb 64
 
-section .text
+segment .text
     global _start
 
 _start:
-    mov rsi, string
-    add rsi, strLen - 1     ; rsi -> last char of source
+    ;write prompt
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, prompt
+    mov rdx, prLen
+    syscall
+
+    ;read input
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input
+    mov rdx, 64
+    syscall
+    mov r12, rax            ; save input length
+
+    mov r13, r12
+    dec r13
+
+    ;write yourString
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, yourString
+    mov rdx, ySLen
+    syscall
+
+    ;write input
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, input
+    mov rdx, r13
+    syscall
+
+    ;write newline
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    ;write ySReversed
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, ySReversed
+    mov rdx, ySRLen
+    syscall
+
+    cmp r13, 0
+    je final_print
+
+pointers:
+    mov rsi, input
+    add rsi, r13           
+    dec rsi                 ; rsi -> last char of source
     mov rdi, revString      ; rdi -> first char of dest
-    mov rcx, strLen         ; rcx -> counter for loop
+    mov rcx, r13            ; rcx -> counter for loop
 
 reverse_loop:
     mov al, [rsi]           ; load last char
@@ -22,11 +79,12 @@ reverse_loop:
     dec rcx                 ; decrease counter
     jnz reverse_loop        ; loop until string is reversed
 
-    ;write syscall
+final_print:
+    ;write revString
     mov rax, 1
     mov rdi, 1
     mov rsi, revString
-    mov rdx, strLen
+    mov rdx, r13
     syscall
 
     ;exit syscall
